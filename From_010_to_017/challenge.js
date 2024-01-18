8f42e8a2ed6ec646bc20e196453b3696fd524710
aehamdy <ae.hamdy91@gmail.com>
2024-01-18 13:12:20 +0200

    Added the challenge

---
+++ From_010_to_017/challenge.js
@@ -0,0 +1,6 @@
+const theTitle = "Elzero", theDesc = "Elzero Web School", theDate = "25/10";
+const component = `<h3>Hello ${theTitle}</h3>
+<p>${theDesc}</p>
+<span>${theDate}</span>`;
+
+document.write(component.repeat(4));
