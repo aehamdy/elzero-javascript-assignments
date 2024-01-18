e739008f30c5dd471d1a5cea028f3c96fe7508a0
aehamdy <ae.hamdy91@gmail.com>
2024-01-18 13:01:19 +0200

    First commit

---
+++ From_010_to_017/script.js
@@ -0,0 +1,28 @@
+// Assignment 1
+const numberOne = 10;
+const numberTwo = 20;
+console.log(numberOne + "" + numberTwo);
+console.log(typeof "1020");
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof `${numberOne}${numberTwo}`);
+console.log(numberTwo + "\n" + numberOne);
+console.log(`${numberTwo}
+${numberOne}`);
+
+
+// Assignment 2
+// The solution is HTML Code
+// <div id="elzero">object</div>
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+
+// Assignment 3
+console.log("\`'I'm In\n \\\\ \n Love \\\\ \"\"\" \'\'\' \n ++ With ++ \n \\ \"\"\"\\\"\"\"\n\"\"JavaScript\"\"\`\`")
+
+
+// Assignment 4
+const a = 21;
+const b = 20;
+let four = Math.ceil(a / b) + !false + true;
+console.log("_" + `${a}_${b}`.repeat(four) + "_");
