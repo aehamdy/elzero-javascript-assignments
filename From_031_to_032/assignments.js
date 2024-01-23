9a0598cae8a6c7fccf2c256c5a55bdf7681ae6c3
aehamdy <ae.hamdy91@gmail.com>
2024-01-23 10:49:43 +0200

    Added Comparison and Logical Operator assignments

---
+++ From_031_to_032/assignments.js
@@ -0,0 +1,24 @@
+// Assignment 1
+
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110 > 100 > 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(typeof -50 === typeof +"-40"); // true
+console.log(typeof 10 === typeof -"-40"); // true
+console.log(typeof "10" !== typeof 10); // true
+console.log(typeof 20 !== false); // true
+
+
+
+// Assignment 2
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 < num2); // true
+console.log(num2 > num1); // true
+console.log(typeof num1 === typeof num2); // true
+console.log(num1 != num2); // true
+console.log(num1 !== num2); // true
+console.log(num1 <= num2); // true
