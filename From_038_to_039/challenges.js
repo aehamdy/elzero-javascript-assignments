136049c5fe10146057df67710554e24dfe47c5f0
aehamdy <ae.hamdy91@gmail.com>
2024-01-25 10:59:54 +0200

    Added switch and if statement's second challenge

--- From_038_to_039/challenges.js
+++ From_038_to_039/challenges.js
@@ -24,3 +24,27 @@ default:
 }
 
 
+// Challenge 2
+
+let holidays = 6;
+let money = 0;
+
+if (holidays === 0) {
+    money = 5000;
+    console.log(`The money is ${money}`);
+} else if (holidays === 1 || holidays === 2) {
+    money = 3000;
+    console.log(`The money is ${money}`);
+} else if (holidays === 3) {
+    money = 2000;
+    console.log(`The money is ${money}`);
+} else if (holidays === 4) {
+    money = 1000;
+    console.log(`The money is ${money}`);
+} else if (holidays === 5) {
+    money = 0;
+    console.log(`There is no money for you`);
+} else {
+    money = 0;
+    console.log(`There is no money for you`);
+}
