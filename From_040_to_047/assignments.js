cdc8356e4a29279bcc3c90befc337bf67f1c4410
aehamdy <ae.hamdy91@gmail.com>
2024-01-27 00:59:55 +0200

    Add the rest assignments of Array

--- From_040_to_047/assignments.js
+++ From_040_to_047/assignments.js
@@ -35,3 +35,39 @@ let finalArr = arrTwo.slice(- arrTwo.length/arrTwo.length).concat(arrOne.reverse
 console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
 
 /* ------------------------------------------- */
+
+
+/* ---------------  Assignment 4  --------------- */
+
+let website = "Go";
+let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+
+console.log(words[website.length][website.length - website.length].slice(website.length).toUpperCase()); // ZERO
+
+/* ------------------------------------------- */
+
+
+/* ---------------  Assignment 5  --------------- */
+
+let needle = "JS";
+let haystack = ["PHP", "JS", "Python"];
+
+// Write 3 Solutions
+if (haystack.includes(needle)) {
+    console.log("Found");
+} else {
+    console.log("Not found");
+}
+
+/* ------------------------------------------- */
+
+
+/* ---------------  Assignment 6  --------------- */
+
+let arr1 = ["A", "C", "X"];
+let arr2 = ["D", "E", "F", "Y"];
+let allArrs = [arr1.pop().toLowerCase(), arr2.pop().toLowerCase(), arr2.pop().toLowerCase()].sort().join("");
+
+// Your Code Here
+
+console.log(allArrs); // fxy
