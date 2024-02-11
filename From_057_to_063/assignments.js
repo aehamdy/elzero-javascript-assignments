9c2b05b8af446951b9aea9863ba1d25ab582eabb
aehamdy <ae.hamdy91@gmail.com>
2024-02-11 19:11:00 +0200

    Added assignment two

--- From_057_to_063/assignments.js
+++ From_057_to_063/assignments.js
@@ -9,13 +9,46 @@ function sayHello(theName, theGender) {
     //     console.log(`Hello ${theName}`);
     // }
 
-    // another solution
+    // another solution using ternary conditional operation
     let gender = "";
-    theGender === "Male" ? gender = "Mr. " : theGender === "Female" ? gender = "Ms. " : gender = "";
+    theGender === "Male"
+    ? gender = "Mr. "
+    : theGender === "Female"
+    ? gender = "Ms. "
+    : gender = "";
+
     console.log(`Hello ${gender}${theName}`);
 }
 
+// Needed Output
+//   sayHello("Osama", "Male"); // "Hello Mr Osama"
+//   sayHello("Eman", "Female"); // "Hello Miss Eman"
+//   sayHello("Sameh"); // "Hello Sameh"
+
+/* ========================================================= */
+
+// Assignment Two
+
+function calculate(firstNum, secondNum, operation) {
+
+    let message;
+
+    secondNum === undefined
+    ? message = `Second number is not found`
+    : operation === "add" || operation === undefined
+    ? message = firstNum + secondNum
+    : operation === "subtract"
+    ? message = firstNum - secondNum
+    : operation === "multiply"
+    ? message = firstNum * secondNum
+    : message = "Something Wrong";
+
+    console.log(message);
+}
+
   // Needed Output
-  sayHello("Osama", "Male"); // "Hello Mr Osama"
-  sayHello("Eman", "Female"); // "Hello Miss Eman"
-  sayHello("Sameh"); // "Hello Sameh"
+  calculate(20); // Second Number Not Found
+  calculate(20, 30); // 50
+  calculate(20, 30, 'add'); // 50
+  calculate(20, 30, 'subtract'); // -10
+  calculate(20, 30, 'multiply'); // 600
