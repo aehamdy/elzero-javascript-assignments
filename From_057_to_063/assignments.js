de62c152325d1a2143c015fc5266505365c33ae0
aehamdy <ae.hamdy91@gmail.com>
2024-02-11 20:25:26 +0200

    Added assignment three

--- From_057_to_063/assignments.js
+++ From_057_to_063/assignments.js
@@ -47,8 +47,26 @@ function calculate(firstNum, secondNum, operation) {
 }
 
   // Needed Output
-  calculate(20); // Second Number Not Found
-  calculate(20, 30); // 50
-  calculate(20, 30, 'add'); // 50
-  calculate(20, 30, 'subtract'); // -10
-  calculate(20, 30, 'multiply'); // 600
+//   calculate(20); // Second Number Not Found
+//   calculate(20, 30); // 50
+//   calculate(20, 30, 'add'); // 50
+//   calculate(20, 30, 'subtract'); // -10
+//   calculate(20, 30, 'multiply'); // 600
+
+/* ========================================================= */
+
+// Assignment three
+
+function ageInTime(theAge) {
+    let months = theAge * 12, weeks = theAge * 48, days = theAge * 365, hours = days * 24, minutes = hours * 60, seconds = minutes * 60;
+
+    if (theAge > 10 && theAge < 100) {
+        console.log(`Your age in months is ${months} months, in weeks is ${weeks} weeks, in days is ${days} days, in hours is ${hours} hours, in minutes is ${minutes} minutes, in seconds is ${seconds} seconds`);
+    } else {
+        console.log("The age is out of range");
+    }
+  }
+  
+  // Needed Output
+  ageInTime(110); // Age Out Of Range
+  ageInTime(38); // Months Example => 456 Months
